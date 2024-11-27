450e73a7ab9e7f276bf14ada851257de95178f7f
Kutman Sulaimanov <kutman.sulaimanov@alatoo.edu.kg>
2024-11-27 19:36:17 +0600

    completing methods

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,54 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> locations = new ArrayList<>();
+        ArrayList<Integer> boxesLocations = new ArrayList<>();
         ArrayList<Integer> guesses = new ArrayList<>();
 
+        System.out.println("Hello, Martians. Let's find your hidden cargo!");
+        int attempts = 0;
+        int maxAttempts = 5;
+
 
     }
-    static void takeGuess(ArrayList<Integer> guesses)
+
+    static int checkGuesses(ArrayList<Integer> boxesLocations, ArrayList<Integer> guesses)
+    {
+        int correct = 0;
+        for(int i = 0; i < 3; i++)
+        {
+            if(boxesLocations.get(i) == guesses.get(i))
+            {
+                correct++;
+            }
+        }
+        return correct;
+    }
+
+    static void takeGuesses(ArrayList<Integer> guesses)
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("")
+        for(int i = 1; i < 4; i++)
+        {
+            System.out.print("Enter your" + i + "guess: ");
+            int guess = sc.nextInt();
+            while(guess < 0 || guess > 7)
+            {
+                System.out.println("Incorrect input. choose an integer between 0 and 7");
+            }
+            guesses.add(guess);
+        }
     }
-    static void generateNumbers(ArrayList<Integer> locations)
+
+    
+    static void generateNumbers(ArrayList<Integer> boxesLocations)
     {
         Random rand = new Random();
-        while (locations.size() < 3)
+        while (boxesLocations.size() < 3)
         {
             int randomNum = rand.nextInt(8);
-            if(!locations.contains(randomNum))
+            if(!boxesLocations.contains(randomNum))
             {
-                locations.add(randomNum);
+                boxesLocations.add(randomNum);
             }
         }
 
