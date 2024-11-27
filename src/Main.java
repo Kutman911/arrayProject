54ca756a1108834845321a3644c66acfa706be18
Kutman Sulaimanov <kutman.sulaimanov@alatoo.edu.kg>
2024-11-27 18:33:24 +0600

    adding methods

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,14 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Integer> locations = new ArrayList<>();
+        ArrayList<Integer> guesses = new ArrayList<>();
 
 
+    }
+    static void takeGuess(ArrayList<Integer> guesses)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("")
     }
     static void generateNumbers(ArrayList<Integer> locations)
     {
